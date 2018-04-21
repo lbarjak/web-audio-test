a49197c6240badfc452f4421eb8c93a9d693bbef
zprodev <zproject.develop@gmail.com>
2018-04-22 01:38:03 +0900

    Add touch decode

--- README.md
+++ README.md
@@ -10,6 +10,8 @@
 
 [Decode(Promise-based syntax)](https://zprodev.github.io/web-audio-test/tests/decode-promise/) [[code](tests/decode-promise/index.js)]
 
+[Decode(Touch start)](https://zprodev.github.io/web-audio-test/tests/decode-touch-start/) [[code](tests/decode-touch-start/index.js)]
+
 [Auto Start](https://zprodev.github.io/web-audio-test/tests/auto-start/) [[code](tests/auto-start/index.js)]
 
 [Auto Start(Play by touch for iOS)](https://zprodev.github.io/web-audio-test/tests/auto-start-touch-play/) [[code](tests/auto-start-touch-play/index.js)]
---
+++ tests/decode-touch-start/index.js
@@ -0,0 +1,17 @@
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const ctx = new AudioContext();
+
+const url = 'https://zprodev.github.io/web-audio-test/assets/Campfire_Song.mp3';
+
+const eventName = typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup';
+document.addEventListener(eventName, () => {
+  const request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+  request.onload =  () => {
+    ctx.decodeAudioData(request.response, (audioBuffer) => {
+      console.log("length:" + audioBuffer.length.toString());
+    });
+  }
+  request.send();
+});
